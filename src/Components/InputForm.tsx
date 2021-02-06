b5bfec2d0891256622cfb2406c0506be159a41f5
hardi shah <hardishahhardi@gmail.com>
2021-02-06 14:38:13 +0530

    Initial Commit

---
+++ src/Components/InputForm.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+type Profile = {
+    firstname: string
+    lastname: string
+    age: number
+  }
+  
+ export default function InputForm() {
+    const {register, handleSubmit, errors} = useForm<Profile>()
+  
+    const onSubmit = handleSubmit((data) => {
+      alert(JSON.stringify(data))
+    })
+  
+    return (
+      <main>
+      <form onSubmit={onSubmit}>
+        <div>
+          <label htmlFor="firstname">First Name</label>
+          <input ref={register({ required: true })} id="firstname" name="firstname" type="text"/>
+          {
+            errors.firstname && <div className="error">Enter your name</div>
+          }
+        </div>
+        <div>
+          <label htmlFor="lastname">Last Name</label>
+          <input ref={register({ required: true })} id="lastname" name="lastname" type="text"/>
+          {
+            errors.lastname && <div className="error">Enter your last name</div>
+          }
+        </div>
+        <div>
+          <label htmlFor="age">Age</label>
+          <input ref={register({ required: true })} id="age" name="age" type="text"/>
+          {
+            errors.age && <div className="error">Enter your age</div>
+          }
+        </div>
+        <button type="submit">Save</button>
+      </form>
+      </main>
+    );
+  }
