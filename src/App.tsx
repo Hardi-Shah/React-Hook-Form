a40b8102887be950dbf5e66b80c9a7f30d2b5f98
hardi shah <hardishahhardi@gmail.com>
2021-02-06 15:17:45 +0530

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import InputForm from './Components/InputForm';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
