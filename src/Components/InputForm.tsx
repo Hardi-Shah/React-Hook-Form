19f43e46983cb78e18ddae51e3d32e3094073c15
hardi shah <hardishahhardi@gmail.com>
2021-02-08 14:45:45 +0530

    Initial Commit

--- src/Components/InputForm.tsx
+++ src/Components/InputForm.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import './InputForm.css'
+import  './InputForm.css'
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
+import classNames from 'classnames/bind'
 
 type InputForm = {
     firstName: string
