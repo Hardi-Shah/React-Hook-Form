85ad4b9661d8152dc569c3bfb5e91b6823ed3e0f
hardi shah <hardishahhardi@gmail.com>
2021-02-09 18:19:01 +0530

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,12 @@ import './App.css';
 import InputForm from './Components/InputForm';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Header from './Components/Header/Header';
-import HeadLine from './Components/HeadLine/HeadLine';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <InputForm />
-        {/* <Header/> */}
-        {/* <section className="main">
-          <HeadLine header="Posts" desc="Click the button to render posts!" />    
-        </section> */}
       </div>
     </Router>
   );
